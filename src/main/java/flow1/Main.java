68f8c4adc2e6e0485058336ea241082d3ffc1861
Salar86 <99397531+Salar86@users.noreply.github.com>
2023-09-05 13:34:50 +0200

    entity relations for Address, Phone and Zip setup

--- src/main/java/flow1/Main.java
+++ src/main/java/flow1/Main.java
@@ -1,8 +1,10 @@
 package flow1;
 
 import flow1.config.HibernateConfig;
+import flow1.model.Address;
 import flow1.model.Phone;
 import flow1.model.Users;
+import flow1.model.Zip;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 
@@ -12,6 +14,10 @@ public class Main {
 
         try (EntityManager em = emf.createEntityManager()) {
             Users u1 = new Users("Preben", "Prebensen", "preben@mail.dk");
+            Address address = new Address("Ferskenvej 80");
+            Zip zip = new Zip(4700, "Næstved", "Region Sjælland", "Næstved Kommune");
+            zip.addAddress(address);
+            address.addUser(u1);
             u1.addPhone("+4555774778");
             em.getTransaction().begin();
             em.persist(u1);
--- src/main/java/flow1/model/Address.java
+++ src/main/java/flow1/model/Address.java
@@ -1,13 +1,13 @@
 package flow1.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -19,6 +19,8 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String address;
+    @ManyToOne (cascade = CascadeType.ALL)
+    private Zip zip;
 
     public Address(String address) {
         this.address = address;
@@ -28,6 +30,7 @@ public class Address {
     private Set<Users> users = new HashSet<>();
 
     public void addUser(Users users){
+        this.users.add(users);
         if(users != null){
             users.setAddress(this);
         }
